bc218847e1648af8c52abe0de77b8b21c361541b
sergey_05 <sergey_psw@tut.by>
2018-09-04 20:28:35 +0300

    Added Mongo repo skeleton

---
+++ DataAccessLayer/MongoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+
+namespace DataAccessLayer
+{
+    public class MongoRepository<T> : IMongoRepository<T> where T : class
+    {
+        private readonly IMongoClient _client;
+
+        public MongoRepository(IMongoClient client)
+        {
+            _client = client;
+        }
+    }
+}
